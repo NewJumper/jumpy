509c1a532387767ed9ae23bc6e7091a87d17b84f
NewJumper <muhtasim.nabi@gmail.com>
2023-12-13 18:46:20 -0600

    added some basic commands

--- src/index.js
+++ src/index.js
@@ -9,12 +9,47 @@ const GUILD_ID = process.env.DEV_GUILD_ID
 const client = new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent ] })
 const rest = new REST({ version: "10" }).setToken(TOKEN)
 
-client.on('ready', () => console.log(client.user.username + " is online"))
 const commands = [
     {
-      name: "ping",
-      description: "pong",
+        name: "add",
+        description: "Add two numbers",
+        options: [
+            {
+                name: "num1",
+                description: "The first number",
+                type: 4,
+                required: true
+            },
+            {
+                name: "num2",
+                description:" The second number",
+                type: 4,
+                required: true
+            }
+        ]
     },
+    {
+        name: "math",
+        description: "Answer a math question",
+        options: [
+            {
+                name: "difficulty",
+                description: "Difficulty",
+                type: 3,
+                required: true,
+                choices: [
+                    {
+                        name: "easy",
+                        value: "easy"
+                    },
+                    {
+                        name: "hard",
+                        value: "hard"
+                    }
+                ]
+            }
+        ]
+    }
 ];
 
 async function main() {
@@ -28,3 +63,21 @@ async function main() {
 }
 
 main()
+client.on('ready', () => console.log(client.user.username + " is online"))
+client.on('interactionCreate', async interaction => {
+    if(!interaction.isChatInputCommand()) return;
+    switch(interaction.commandName) {
+        case "add": {
+            let num1 = interaction.options.getInteger("num1")
+            let num2 = interaction.options.getInteger("num2")
+            await interaction.reply(num1 + " + " + num2 + " = " + (num1 + num2))
+            return
+        }
+        case "math": {
+            let difficulty = interaction.options.getString("difficulty")
+            if(difficulty == "easy") await interaction.reply("what is `1 + 1`")
+            else await interaction.reply("what is the integral of `x^x`")
+            return
+        }
+    }
+})
